4b7fce43216dc2da3525bc76f9d9971cdebb10b1
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-15 14:14:22 +0200

    Refactor a context provider with hooks

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,27 +1,16 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { language: 'english' };
-		this.changeLanguage = this.changeLanguage.bind(this);
-	}
+export function LanguageProvider(props) {
+	const [language, setLanguage] = useState('english');
+	const changeLanguage = (e) => setLanguage(e.target.value);
 
-	changeLanguage(e) {
-		this.setState({ language: e.target.value });
-	}
-
-	render() {
-		return (
-			<LanguageContext.Provider
-				value={{ ...this.state, changeLanguage: this.changeLanguage }}
-			>
-				{this.props.children}
-			</LanguageContext.Provider>
-		);
-	}
+	return (
+		<LanguageContext.Provider value={{ language, changeLanguage }}>
+			{props.children}
+		</LanguageContext.Provider>
+	);
 }
 
 // create high order component, which takes different Component as an argument
